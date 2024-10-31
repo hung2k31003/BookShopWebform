82c781863c70e7762ba928c35df6c84fc904fb91
hung2k31003 <hoanghung2k3kd@gmail.com>
2024-10-31 21:15:25 +0700

    Book Shop Webform

---
+++ Login.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        BaiTapLonContext db = new BaiTapLonContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            var cus = db.Customer.FirstOrDefault(p => p.Email == txtEmail.Text && p.Password == txtPassword.Text);
+            if (cus != null)
+            {
+                Session["login"] = cus;
+                if (cus.Role == "admin")
+                {
+                    Response.Redirect("Admin.aspx");
+                }
+                else
+                {
+                    Response.Redirect("Home.aspx");
+                }
+                msgLogin.Text = "";
+            }
+            else
+            {
+                msgLogin.Text = "Tên đăng nhập hoặc mật khẩu không đúng!!!";
+            }
+        }
+    }
+}
